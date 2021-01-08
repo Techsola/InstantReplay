b5f7d741988815931872e22d83a0a52812466108
jnm2 <me@jnm2.com>
2021-01-08 18:57:22 -0500

    Add ApiContractGenerator

---
+++ src/pubapi/Techsola.InstantReplay.net5.0-windows.pubapi.cs
@@ -0,0 +1,14 @@
+// Name:       Techsola.InstantReplay
+// Public key: ACQAAASAAACUAAAABgIAAAAkAABSU0ExAAQAAAEAAQCF2qEmEG2zcnZ9BXuFt5bceeQvKHInNQu9VrBRSiMnuMwhRAJuwrvTnvHRGdPK7qVsMGr70swPONPIRkVycJ12TETXbPKCWiFGykF6rz3IuLIOnplG+bWku3XokRpGm48MTanrPtdl0vZz1TsRsFLYNWsekO+TzilzmW/QI+CvwA==
+
+namespace Techsola.InstantReplay
+{
+    [System.Runtime.CompilerServices.NullableContext(1)]
+    [System.Runtime.CompilerServices.Nullable(0)]
+    public static class InstantReplayCamera
+    {
+        public static void SaveGif(System.IO.Stream stream);
+
+        public static void Start();
+    }
+}
